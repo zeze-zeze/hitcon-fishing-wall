ab7f356e18d2537b09697f707a06f1bd13fd35f7
zeze <zezectf@gmail.com>
2023-05-21 16:41:11 +0800

    feat: communicatation between fish and dashboard

--- dashboard/server.js
+++ dashboard/server.js
@@ -6,78 +6,101 @@ const fs = require('fs');
 const port = 5002
 process.env.TZ = 'Asia/Taipei';
 const app = express();
+const activities = ['fish', 'ctf', 'badge', 'geocaching'];
 const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-if (!fs.existsSync('userdata')) {
-    fs.mkdirSync('userdata', 0744);
+app.use(express.static(path.join(__dirname, './')));
+
+// Initialize user data with the data stored in ./userdata.
+if (!fs.existsSync('./userdata')) {
+    fs.mkdirSync('./userdata', 0744);
 }
+const userdata = {};
+activities.forEach(activity => {
+    if (!fs.existsSync(`./userdata/${activity}`)) {
+        fs.mkdirSync(`./userdata/${activity}`, 0744);
+    }   
 
-// app.use(express.static(path.join(__dirname, '../frontend')));
+    userdata[activity] = [];
+    fs.readdir(`./userdata/${activity}`, (err, files) => {
+        console.log(files);
+        files.forEach(file => {
+            fs.readFile(`./userdata/${activity}/${file}`, 'utf8', (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log(JSON.parse(data));
+                userdata[activity].push(JSON.parse(data));
+            });
+        });
+    });
+});
 
-app.post('/fishing', urlencodedParser, function(request, response){
-    const host = request.headers.host
+app.post('/fish', urlencodedParser, function(req, res){
+    const host = req.headers.host;
     console.log(host);
-    console.log(request.url);
-    console.log(request.body);
+    console.log(req.url);
+    console.log(req.body);
     const date = new Date();
     const time = '' + date.toLocaleTimeString();
-    const username = request.body.username;
-    const data = JSON.stringify({'time': time, 'username': username});
-    console.log(data);
-    fs.writeFile(`userdata/${username}.json`, data, err => {
+    const username = req.body.username;
+
+    var description;
+    if (typeof(req.body.description) === 'string') {
+        description = req.body.description;
+    }
+    else {
+        description = 'QAQ';
+    }
+    
+    const data = {'time': time, 'username': username, 'description': description};
+    console.log(JSON.stringify(data));
+
+    if (!fs.existsSync(`./userdata/fish/${username}.json`)) {
+        userdata['fish'].push(data);
+    }
+    fs.writeFile(`./userdata/fish/${username}.json`, JSON.stringify(data), err => {
         if (err) {
             console.error(err);
         }
     });
-    response.write(data);
+
+    res.header("Content-Type", "application/json; charset=utf-8");
+    res.end(JSON.stringify(data));
 });
 
-app.get('/fishing', function(request, response){
+app.post('/ctf', function(req, res){
     
 });
 
-app.post('/ctf', function(request, response){
+app.post('/badge', function(req, res){
     
 });
 
-app.get('/ctf', function(request, response){
+app.post('/geocaching', function(req, res){
     
 });
 
-app.post('/badge', function(request, response){
+app.get('/dashboard', function(req, res){
+    const category = req.query.category;
+    res.header("Content-Type", "application/json; charset=utf-8");
     
-});
-
-app.get('/badge', function(request, response){
-    
-});
-
-app.post('/geocaching', function(request, response){
-    
-});
-
-app.get('/geocaching', function(request, response){
-    
-});
-
-app.get('/dashboard', function(request, response){
-    const category = request.query.category;
-    console.log(category);
-    if (category == 'fishing'){
-
+    if (category == 'fish'){
+        res.end(JSON.stringify(userdata['fish']));
     }
     else if (category == 'ctf'){
-
+        res.end(JSON.stringify(userdata['ctf']));
     }
     else if (category == 'badge'){
-
+        res.end(JSON.stringify(userdata['badge']));
     }
     else if (category == 'geocaching'){
-
+        res.end(JSON.stringify(userdata['geocaching']));
     }
     else {
-
+        res.end('unknown category');
     }
 });
 
