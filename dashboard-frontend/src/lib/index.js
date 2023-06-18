126324804c1662c778a92a2027d5de648acba72c
doraeric <benson.doraemon@gmail.com>
2023-06-18 23:30:20 +0800

    feat: add different pages and table display

---
+++ dashboard-frontend/src/lib/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {{
+ *   headers: string[],
+ *   data: {[key: string]: any}[],
+ * }}
+ */
+export function jsonToTable({ headers, data }) {
+  const rows = data.map((item) => {
+    return headers.map((key) => item[key]);
+  });
+  return { headers, data: rows };
+}
--- dashboard-frontend/src/main.jsx
+++ dashboard-frontend/src/main.jsx
@@ -1,9 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root, { RootIndex } from "./pages/Root";
+import FishPage from "./pages/Fish";
+import BadgePage from "./pages/Badge";
+import CtfPage from "./pages/CTF";
+import GeocachingPage from "./pages/Geocaching";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <RootIndex />,
+      },
+      {
+        path: "fish",
+        element: <FishPage />,
+      },
+      {
+        path: "badge",
+        element: <BadgePage />,
+      },
+      {
+        path: "ctf",
+        element: <CtfPage />,
+      },
+      {
+        path: "geocaching",
+        element: <GeocachingPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
