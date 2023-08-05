adf91b6c7b3a37f1ec390aa6af568695fef9e139
doraeric <benson.doraemon@gmail.com>
2023-08-05 23:38:42 +0800

    feat: add 3 badge reset APIs

--- dashboard-backend/common/index.js
+++ dashboard-backend/common/index.js
@@ -2,6 +2,11 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+// https://github.com/prisma/prisma/issues/5026#issuecomment-759596097
+// prisma.$on("query", async (e) => {
+//   console.log(`${e.query} ${e.params}`);
+// });
+
 module.exports = {
   prisma,
 };
--- dashboard-backend/routes/badge/index.js
+++ dashboard-backend/routes/badge/index.js
@@ -6,12 +6,16 @@ const {
 const { apiKeyAuth } = require("../../middlewares/auth");
 const {
   getEmojiWithUser,
+  checkNonExistentUids,
   createOrUpdatePopcat,
   getPopcatWithUser,
   createEmojiRecord,
   createOrUpdateBadgeInfo,
   getDinoWithUser,
   createOrUpdateDino,
+  deleteAndCreatePopcats,
+  deleteAndCreateDinos,
+  deleteAndCreateEmoji,
 } = require("./handlers");
 
 const router = express.Router();
@@ -43,6 +47,57 @@ router.get("/popcat", async (req, res) => {
 });
 
 router.put(
+  "/popcat/all",
+  apiKeyAuth,
+  SchemaValidator("put_all_popcat"),
+  async (req, res) => {
+    /*
+      #swagger.operationId = "put_all_popcat"
+      #swagger.parameters["body"] = {
+        in: "body",
+        "@schema": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "card_uid": {
+                "type": "string",
+                "pattern": "[0-9a-fA-F]{8}",
+              },
+              "score": {
+                "type": "number",
+              },
+            },
+            "required": ["card_uid", "score"]
+          },
+        }
+      }
+    */
+    const data = req.body.map(({ card_uid, score }) => ({
+      cardUid: Buffer.from(card_uid, "hex"),
+      score,
+    }));
+    try {
+      const result = await deleteAndCreatePopcats(data);
+      res.status(201).json(result);
+    } catch (error) {
+      const errorType = error.constructor.name;
+      if (errorType.startsWith("PrismaClient")) {
+        const errorMsg = { error: errorType };
+        if ("code" in error) errorMsg.code = error.code;
+        if (typeof error.message === "string") {
+          errorMsg.message = error.message.split("\n").pop();
+        }
+        res.status(400).json(errorMsg);
+      } else {
+        console.error(error);
+        res.status(500).send();
+      }
+    }
+  }
+);
+
+router.put(
   "/popcat/:cardUid",
   apiKeyAuth,
   computeCardUidPath,
@@ -66,6 +121,57 @@ router.get("/dino", async (req, res) => {
   const record = await getDinoWithUser();
   res.json(record);
 });
+
+router.put(
+  "/dino/all",
+  apiKeyAuth,
+  SchemaValidator("put_all_dino"),
+  async (req, res) => {
+    /*
+      #swagger.operationId = "put_all_dino"
+      #swagger.parameters["body"] = {
+        in: "body",
+        "@schema": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "card_uid": {
+                "type": "string",
+                "pattern": "[0-9a-fA-F]{8}",
+              },
+              "score": {
+                "type": "number",
+              },
+            },
+            "required": ["card_uid", "score"]
+          },
+        }
+      }
+    */
+    const data = req.body.map(({ card_uid, score }) => ({
+      cardUid: Buffer.from(card_uid, "hex"),
+      score,
+    }));
+    try {
+      const result = await deleteAndCreateDinos(data);
+      res.status(201).json(result);
+    } catch (error) {
+      const errorType = error.constructor.name;
+      if (errorType.startsWith("PrismaClient")) {
+        const errorMsg = { error: errorType };
+        if ("code" in error) errorMsg.code = error.code;
+        if (typeof error.message === "string") {
+          errorMsg.message = error.message.split("\n").pop();
+        }
+        res.status(400).json(errorMsg);
+      } else {
+        console.error(error);
+        res.status(500).send();
+      }
+    }
+  }
+);
 
 router.put(
   "/dino/:cardUid",
@@ -93,6 +199,72 @@ router.get("/emoji", async (req, res) => {
   const data = await getEmojiWithUser();
   res.json(data);
 });
+
+router.put(
+  "/emoji/all",
+  apiKeyAuth,
+  SchemaValidator("put_all_emoji"),
+  async (req, res) => {
+    /*
+      #swagger.operationId = "put_all_emoji"
+      #swagger.parameters["body"] = {
+        in: "body",
+        "@schema": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "card_uid": {
+                "type": "string",
+                "pattern": "[0-9a-fA-F]{8}",
+              },
+              "content": {
+                "type": "string",
+              },
+              "timestamp": {
+                "type": "string",
+              },
+            },
+            "required": ["card_uid", "content", "timestamp"]
+          },
+        }
+      }
+    */
+    const data = req.body.map(({ card_uid, content, timestamp }) => ({
+      cardUid: Buffer.from(card_uid, "hex"),
+      content,
+      timestamp: new Date(timestamp),
+    }));
+    const uids = data.map((i) => i.cardUid);
+    try {
+      // check foreign key
+      const missing = await checkNonExistentUids({ uids });
+      if (missing.length > 0) {
+        return res.status(400).json({
+          error: "card not registerd",
+          uids: missing.map((i) => i.uid.toString("hex")),
+        });
+      }
+
+      const result = await deleteAndCreateEmoji(data);
+      res.status(201).json(result);
+    } catch (error) {
+      console.log(error);
+      const errorType = error.constructor.name;
+      if (errorType.startsWith("PrismaClient")) {
+        const errorMsg = { error: errorType };
+        if ("code" in error) errorMsg.code = error.code;
+        if (typeof error.message === "string") {
+          errorMsg.message = error.message.split("\n").pop();
+        }
+        res.status(400).json(errorMsg);
+      } else {
+        console.error(error);
+        res.status(500).send();
+      }
+    }
+  }
+);
 
 router.post(
   "/emoji/:cardUid",
