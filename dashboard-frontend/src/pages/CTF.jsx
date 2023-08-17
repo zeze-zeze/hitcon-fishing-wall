98a26f36f936d6afd53713b2fc583090e1dbe776
cjtp01 <root@cjtp01.zuan.im>
2023-08-17 23:22:56 +0800

    Change rectf scoreboard

--- dashboard-frontend/src/pages/CTF.jsx
+++ dashboard-frontend/src/pages/CTF.jsx
@@ -3,7 +3,7 @@ export default function CtfPage() {
     <div className="absolute inset-0 h-[calc(100vh-6rem)]">
       <iframe
         className="w-full h-full"
-        src="https://rectf.hitcon2023.online/scoreboard"
+        src="https://rectf.hitcon2023.online/scoreboard_iframe"
       />
     </div>
   );
